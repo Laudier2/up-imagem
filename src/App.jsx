64299672dacaaa47c3dc4ee0d309f5eb07fb8660
Laudier2 <trabalhandooline2019@hotmail.com>
2022-07-11 19:24:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+//import Home from './components/index'
+
+const lista = [
+  {
+    name: "Laudier",
+    idade: 37,
+    sexo: "Masculino"
+  }
+]
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="h1">Isso é um teste</h1>
+        <hr />
+        <h5 className="h5 mt-2">Dados do cliente</h5>
+        {
+          lista.map(e => (
+            <ul key={e}>
+              <li>{e.name}</li>
+              <li>{e.idade}</li>
+              <li>{e.sexo}</li>
+            </ul>
+          ))
+        }
+      </div>
+    </>
+  );
+}
+
+export default App;
